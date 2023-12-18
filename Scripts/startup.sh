ad7cc2005732ae49a94cabb0d015e582d53874f4
n0x1u5 <toteking1520@gmail.com>
2023-12-18 16:29:50 +0100

    Update startup.sh

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo "> > > > > > > > Configuring keys ESP: "
+echo -e "\e[1;33m> > > > > > > > Configuring keys ESP: \e[0m"
 setxkbmap es
 
-echo "[v] Done!"
+echo -e "\e[1;32m[v] Done! \e[0m"
 
-echo "> > > > > > > > Updating Kali-Linux: "
+echo -e "\e[1;33m> > > > > > > > Checking Screenfetch... \e[0m"
+
+sudo apt install screenfetch -y
+
+echo -e "\e[1;32m[v] Done! Screenfetch is ready. \e[0m"
+
+echo -e "\e[1;33m> > > > > > > > Updating Kali-Linux: \e[0m"
 sudo apt update
 sudo apt -y full-upgrade
 sudo apt -y autoremove
 lsb_release -a
+screenfetch
 
-echo "[v] STARTUP COMPLETED SUCCESSFULLY!"
-
+echo -e "\e[1;35m[\e[0m\e[1;37m*\e[0m\e[1;35m] STARTUP COMPLETED SUCCESSFULLY! \e[0m"
